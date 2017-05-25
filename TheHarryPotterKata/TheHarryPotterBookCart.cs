b9f3d24991740e60a41dc2a75e6ab2c039f59ee3
Kelsey Beard <kbeard@apterainc.com>
2017-05-25 00:19:33 -0400

    Refactor code Tests Still passing

--- TheHarryPotterKata/TheHarryPotterBookCart.cs
+++ TheHarryPotterKata/TheHarryPotterBookCart.cs
@@ -6,23 +6,16 @@ namespace TheHarryPotterKata
 	[TestFixture]
 	public class TheHarryPotterBookCart
 	{
-		//private HarryPotterBookCart shoppingCart;
-		//[SetUp]
-		//public void SetUp()
-		//{
-		//	shoppingCart = new HarryPotterBookCart();
-		//}
+		private static HarryPotterBookCart shoppingCart;
+		[SetUp]
+		public void SetUp()
+		{
+			shoppingCart = new HarryPotterBookCart();
+		}
 		
-		//[Test]
-		//public void Given0BooksTotalIsEqualTo0()
-		//{
-		//	var total = shoppingCart.Add();
-		//	Assert.AreEqual(0, total);
-		//}
 		[Test]
 		public void Given0BooksTotalIsEqualTo0()
-		{
-			var shoppingCart = new HarryPotterBookCart();
+		{;
 			var total = shoppingCart.Add();
 			Assert.AreEqual(0, total);
 		}
@@ -30,7 +23,6 @@ namespace TheHarryPotterKata
 		[Test]
 		public void Given1BookThenTotalIsEqualTo8()
 		{
-			var shoppingCart = new HarryPotterBookCart();
 			var total = shoppingCart.Add("The Sorcerer's Stone");
 			Assert.AreEqual(8, total);
 		}
